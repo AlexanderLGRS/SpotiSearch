31818a23250e8f92cc29d61aeac61fc443c42ca6
Alexander Lansdorp <alexlansdorprs@gmail.com>
2022-03-12 16:20:08 -0500

    Updates

---
+++ precache-manifest.f675ab4d2c48c5c6095e5bb0c8a0fd7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83d18dfd77822f70d469",
+    "url": "/SpotiSearch/static/css/main.eb86b2d0.chunk.css"
+  },
+  {
+    "revision": "83d18dfd77822f70d469",
+    "url": "/SpotiSearch/static/js/main.17d9c6b8.chunk.js"
+  },
+  {
+    "revision": "e19ebb55f75b1f01f41a",
+    "url": "/SpotiSearch/static/js/runtime~main.6b3e0eb0.js"
+  },
+  {
+    "revision": "f2e423dc8840fc39354b",
+    "url": "/SpotiSearch/static/js/2.ca6b1062.chunk.js"
+  },
+  {
+    "revision": "0222b4ed3fafae3e17ab",
+    "url": "/SpotiSearch/static/js/3.50b2d2e5.chunk.js"
+  },
+  {
+    "revision": "4de1daf1aa2c5e2467972c920175c110",
+    "url": "/SpotiSearch/static/media/Background2.4de1daf1.jpg"
+  },
+  {
+    "revision": "e695f93f57554a418d77b56dff930ba9",
+    "url": "/SpotiSearch/index.html"
+  }
+];
